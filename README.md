d9c15e2597c2955ec88660f1f9e780bc25478f1e
Jiankun Dong <doctorbobdong0107@gmail.com>
2025-01-15 13:42:03 -0500

    Including blender scripts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BlenderScripts
 From blender file, add and change color, change and add camera angles, and out comes mp4 files
+
+# Credit
+Creator: Xiang Zhang
+Updated and Adapted: Jiankun Dong
---
+++ filter_videos.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+# Define source and destination directories
+source_directory = "../MP4s/MultiCameras/Jan13"
+destination_directory = "../MP4s/MultiCameras/Z200_Videos"
+
+# Ensure the destination directory exists
+os.makedirs(destination_directory, exist_ok=True)
+
+# Loop through the activities and process videos
+for activity_name in os.listdir(source_directory):
+    
+    if activity_name.endswith(".json"):
+        continue
+    print(f"Processing {activity_name}...")
+    activity_path = os.path.join(source_directory, activity_name)
+    base_name = activity_name.split(".")[0]
+    # Ensure it's a directory
+    if os.path.isdir(activity_path):
+        for file_name in os.listdir(activity_path):
+            
+            # Check if the file matches the naming convention and Z value
+            if file_name.startswith(f"video_{base_name}") and "_Z200_" in file_name:
+                full_file_path = os.path.join(activity_path, file_name)
+
+                # Move the file to the destination folder
+                shutil.copy(full_file_path, os.path.join(destination_directory, file_name))
+
+print(f"All videos with Z=200 have been moved to {destination_directory}.")
